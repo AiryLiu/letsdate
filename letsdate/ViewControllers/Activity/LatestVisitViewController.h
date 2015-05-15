0b625d8f41383b41af5614c3f5daf2a8f2702c3e
liuxuan <liuxuan@ucfgroup.com>
2015-05-15 19:43:07 +0800

    提交动态页

---
+++ letsdate/ViewControllers/Activity/LatestVisitViewController.h
@@ -0,0 +1,13 @@
+//
+//  LatestVisitViewController.h
+//  letsdate
+//
+//  Created by LiuX on 15/5/15.
+//  Copyright (c) 2015年 Maitang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LatestVisitViewController : BaseViewController
+
+@end

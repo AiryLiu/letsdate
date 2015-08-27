40be7e5ae3ad0b73e63037b2aa9f8e5fa48f507b
liuxuan <liuxuan@ucfgroup.com>
2015-08-27 18:45:13 +0800

    提交修改

---
+++ letsdate/ViewControllers/Me/ResetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResetPasswordViewController.h
+//  letsdate
+//
+//  Created by LiuX on 15/8/27.
+//  Copyright © 2015年 Maitang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ResetPasswordViewController : BaseViewController
+
+@end
---
+++ letsdate/ViewControllers/Me/ResetPasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResetPasswordViewController.m
+//  letsdate
+//
+//  Created by LiuX on 15/8/27.
+//  Copyright © 2015年 Maitang. All rights reserved.
+//
+
+#import "ResetPasswordViewController.h"
+
+@interface ResetPasswordViewController ()
+
+@end
+
+@implementation ResetPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
